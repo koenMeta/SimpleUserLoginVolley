07f6e38dfb7ea15d6302f73f6a287d62ff3d4fd0
User <user@mail.com>
2021-12-07 14:53:54 +0100

    Post works with https://jsonplaceholder.typicode.com/posts

--- app/src/main/java/com/training/simpleuserloginvolley/MainActivity.java
+++ app/src/main/java/com/training/simpleuserloginvolley/MainActivity.java
@@ -5,27 +5,33 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.text.method.LinkMovementMethod;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.android.volley.NetworkResponse;
+import com.android.volley.ParseError;
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
+import com.android.volley.toolbox.HttpHeaderParser;
 import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.Volley;
 
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.io.UnsupportedEncodingException;
+
 public class MainActivity extends AppCompatActivity {
 
     private RequestQueue requestQueue;
 
-    private String baseUrl = "";
+    private String baseUrl = "https://40968f05-5a2b-492f-8ef2-a8d667146b83.mock.pstmn.io";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,7 +44,7 @@ public class MainActivity extends AppCompatActivity {
         EditText passwordField = findViewById(R.id.txtPwd);
         Button btnLogin = findViewById(R.id.btnLogin);
 
-        TextView register = (TextView)findViewById(R.id.lnkRegister);
+        TextView register = (TextView) findViewById(R.id.lnkRegister);
         register.setMovementMethod(LinkMovementMethod.getInstance());
 
         register.setOnClickListener(new View.OnClickListener() {
@@ -55,9 +61,10 @@ public class MainActivity extends AppCompatActivity {
                 String email = emailField.getText().toString();
                 String password = passwordField.getText().toString();
 
-                Toast.makeText(MainActivity.this, "Email: " + email + " Password:" + password, Toast.LENGTH_LONG).show();
+//                Toast.makeText(MainActivity.this, "Email: " + email + " Password:" + password, Toast.LENGTH_LONG).show();
+                postData(email, password);
 
-
+//                getData();
             }
         });
     }
@@ -68,37 +75,40 @@ public class MainActivity extends AppCompatActivity {
         JSONObject object = new JSONObject();
         try {
             //input your API parameters
-            object.put("email",email);
-            object.put("password","password");
+            object.put("title", email);
+            object.put("body", "password");
+
         } catch (JSONException e) {
             e.printStackTrace();
         }
         // Enter the correct url for your api service site
-//        String url = getResources().getString(R.string.url);
-        String url = baseUrl + "/api/login";
-        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.POST, url, object,
+//        String url = baseUrl + "/api/login";
+        String url = "https://jsonplaceholder.typicode.com/posts";
+        CustomJsonObjectRequest jsonObjectRequest = new CustomJsonObjectRequest(Request.Method.POST, url, object,
                 new Response.Listener<JSONObject>() {
                     @Override
                     public void onResponse(JSONObject response) {
-                        Toast.makeText(getApplicationContext(), "String Resonse: " + response.toString(), Toast.LENGTH_SHORT).show();
+                        Toast.makeText(getApplicationContext(), "String Response: " + response.toString(), Toast.LENGTH_SHORT).show();
+                        Log.d("JsonObjectRequest", "String Response: " + response.toString());
 //                        resultTextView.setText("String Response : "+ response.toString());
                     }
                 }, new Response.ErrorListener() {
             @Override
             public void onErrorResponse(VolleyError error) {
                 Toast.makeText(getApplicationContext(), "Error getting response", Toast.LENGTH_LONG).show();
+                Log.d("JsonObjectRequest", "String Response: " + error.getLocalizedMessage());
             }
         });
         requestQueue.add(jsonObjectRequest);
     }
 
     // Get Request For JSONObject
-    public void getData(){
+    public void getData() {
         RequestQueue requestQueue = Volley.newRequestQueue(getApplicationContext());
         try {
-            String url = baseUrl + "/api/login";
+            String url = "https://jsonplaceholder.typicode.com/users";
             JSONObject object = new JSONObject();
-            JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.GET, url, null, new Response.Listener<JSONObject>() {
+            CustomJsonObjectRequest jsonObjectRequest = new CustomJsonObjectRequest(Request.Method.GET, url, null, new Response.Listener<JSONObject>() {
                 @Override
                 public void onResponse(JSONObject response) {
 //                    resultTextView.setText("Response : " + response.toString());
@@ -107,12 +117,45 @@ public class MainActivity extends AppCompatActivity {
             }, new Response.ErrorListener() {
                 @Override
                 public void onErrorResponse(VolleyError error) {
-                    Toast.makeText(getApplicationContext(), "Error", Toast.LENGTH_LONG).show();
+                    Toast.makeText(getApplicationContext(), "Error: " + error.getLocalizedMessage(), Toast.LENGTH_LONG).show();
                 }
             });
             requestQueue.add(jsonObjectRequest);
         } catch (Exception e) {
             e.printStackTrace();
+        }
+    }
+
+
+    public class CustomJsonObjectRequest extends JsonObjectRequest {
+
+        public CustomJsonObjectRequest(int method, String url, JSONObject jsonRequest, Response.Listener<JSONObject> listener, Response.ErrorListener errorListener) {
+            super(method, url, jsonRequest, listener, errorListener);
+        }
+
+        public CustomJsonObjectRequest(String url, JSONObject jsonRequest, Response.Listener<JSONObject> listener, Response.ErrorListener errorListener) {
+            super(url, jsonRequest, listener, errorListener);
+        }
+
+        @Override
+        protected Response<JSONObject> parseNetworkResponse(NetworkResponse response) {
+            try {
+                String jsonString = new String(response.data,
+                        HttpHeaderParser.parseCharset(response.headers));
+
+                JSONObject result = null;
+
+                if (jsonString != null && jsonString.length() > 0)
+                    result = new JSONObject(jsonString);
+
+                return Response.success(result,
+                        HttpHeaderParser.parseCacheHeaders(response));
+            } catch (UnsupportedEncodingException e) {
+                return Response.error(new ParseError(e));
+            } catch (JSONException je) {
+                return Response.error(new ParseError(je));
+
+            }
         }
     }
 }
